8e1eee03cfee7a2c726b5090cef3a02e2e8d2a1d
rick <rick20001010@gmail.com>
2021-05-31 02:33:56 +0800

    modify 20210531

--- 03b-compiler2/compiler.c
+++ 03b-compiler2/compiler.c
@@ -5,7 +5,7 @@ int E();
 void STMT();
 void IF();
 void BLOCK();
-
+void For();
 int tempIdx = 0, labelIdx = 0;
 
 #define nextTemp() (tempIdx++)
@@ -88,24 +88,50 @@ void WHILE() {
   emit("goto L%d\n", whileBegin);
   emit("(L%d)\n", whileEnd);
 }
-//參考while寫出
-void IF(){
-  int ifBigin = nextLabel();
-  int ifEnd = nextLabel();
+
+// if (EXP) STMT (else STMT)?
+void IF() {
+  int ifBigin = nextLabel();//開始的label
+  int ifMid = nextLabel();
+  int ifEnd = nextLabel();//結束的label
   emit("(L%d)\n",ifBigin);
   skip("if");
   skip("(");
-  int e = E();
-  emit("if not T%d goto L%d\n", e, ifEnd);
+  int e= E();
+  emit("if not t%d goto L%d\n",e,ifMid);
   skip(")");
-  emit("(L%d)\n", ifEnd);
+  STMT();
+  emit("goto L%d\n",ifEnd);
+  emit("(L%d)\n", ifMid);
+  if (isNext("else")) {
+    skip("else");
+    //emit("if L%d goto L%d\n",ifMid,ifEnd);
+    STMT();
+    emit("(L%d)\n",ifEnd);
+  }
+}
+void FOR() { 
+  int forBegin = nextLabel(); //開始的label
+  int forEnd = nextLabel();   //結束的label
+  emit("(L%d)\n",forBegin);
+  skip("for");
+  skip("(");
+  ASSIGN();                  //i = 0; 在這邊沒有skip(";");是因為在ASSUGN()這個函式中已經有做了
+  int e2 = E();              //i <= 10; 處理裡面的符號
+  emit("if not t%d goto L%d\n",e2,forEnd);
+  skip(";");
+  int e3 = E();
+  skip(")");
+  STMT();
+  emit("goto L%d\n",forBegin);
+  emit("(L%d)\n",forEnd);
 }
 // STMT = WHILE | BLOCK | ASSIGN
 void STMT() {
   if (isNext("while"))
     return WHILE();
-  // else if (isNext("if"))
-  //   IF();
+  else if (isNext("if"))
+    IF();
   else if (isNext("{"))
     BLOCK();
   else
